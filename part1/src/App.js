144eb8d4abac88fc7e1ba91c2bc5a055e5051ba0
Dallas Young <dallasyoung@outlook.com>
2023-06-21 10:32:06 -0600

    Remove unnecessary files, small tweaks to the example React App

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+const Friend = (props) => (
+  <>
+    <p>My friend named {props.name}</p>
+  </>
+)
 
-function App() {
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="friends">
+    { friends.map((name)=>(<Friend name={name} />)) }
     </div>
   );
 }
