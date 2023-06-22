0ce2344c4b8bae92ba69c2e04b22257f22bd82f5
Dallas Young <dallasyoung@outlook.com>
2023-06-22 11:28:42 -0600

    Update `run.bat`

--- part1/course information/run.bat
+++ part1/course information/run.bat
@@ -1,4 +1,4 @@
 set PROJECT_NAME=fullstackopen-part1
-set DOCKERFILE=Dockerfile.prod
+set DOCKERFILE=Dockerfile
 
 docker build -f %DOCKERFILE% -t %PROJECT_NAME% . && docker run --rm -it -p 3000:3000 %PROJECT_NAME%
