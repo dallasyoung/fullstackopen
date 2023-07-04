5c564fa793e76484e96317bd28937298918bf774
Dallas Young <dallasyoung@outlook.com>
2023-07-03 21:10:25 -0600

    Complete exercise 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -16,6 +16,7 @@ const App = () => {
   // My solution before reading the hints included in the exercise instructions
   // const [votes, updateVotes] = useState(Array(anecdotes.length).fill().map(_ => 0));
   const [votes, updateVotes] = useState(new Uint8Array(anecdotes.length));
+  const [highest, updateHighest] = useState(0);
   
   const selectRandom = () => {
     const newRandomNumber = Math.floor(Math.random() * anecdotes.length);
@@ -25,23 +26,58 @@ const App = () => {
   const vote = () => {
     let newVotes = [...votes]; // Careful not to modify the 'votes' React state directly here
     newVotes[selected] += 1;
+    let score = newVotes[selected];
+    let testHighest = votes[highest];
+    if(score > testHighest) {
+      updateHighest(selected);
+    }
     updateVotes(newVotes);
   };
 
-  return (
-    <div>
+  // This conditional rendering code would be much neater if I was properly
+  // using smaller subcomponents like we've done previously, I know
+  if(votes.reduce((a, v) => a + v, 0) > 0) {
+    return (
       <div>
-        {anecdotes[selected]}
+        <div>
+          <h1>Anecdote of the day</h1>
+          <p>{anecdotes[selected]}</p>        
+        </div>
+        <div>
+          <p>Votes: {votes[selected]}</p>
+        </div>
+        <div>
+          <button onClick={selectRandom}>Random anectode</button>
+          <button onClick={vote}>Vote for anectode</button>
+        </div>
+        <div>
+          <h1>Highest-voted Anecdote</h1>
+          <p>{anecdotes[highest]}</p>
+          <p>Votes: {votes[highest]}</p>
+        </div>
       </div>
+    );
+  } else {
+    return (
       <div>
-        <p>Votes: {votes[selected]}</p>
+        <div>
+          <h1>Anecdote of the day</h1>
+          <p>{anecdotes[selected]}</p>        
+        </div>
+        <div>
+          <p>Votes: {votes[selected]}</p>
+        </div>
+        <div>
+          <button onClick={selectRandom}>Random anectode</button>
+          <button onClick={vote}>Vote for anectode</button>
+        </div>
+        <div>
+          <h1>Highest-voted Anecdote</h1>
+          <p>No votes yet</p>
+        </div>
       </div>
-      <div>
-      <button onClick={selectRandom}>Random anectode</button>
-      <button onClick={vote}>Vote for anectode</button>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
