ebaf24973631e5fa93039833cc9943d6067471da
Dallas Young <dallasyoung@outlook.com>
2023-07-03 19:11:34 -0600

    Update courseinfo dev run script

--- part1/course information/run.bat
+++ part1/course information/run.bat
@@ -1,4 +1,4 @@
-set PROJECT_NAME=fullstackopen-part1-dev
+set PROJECT_NAME=fullstackopen-part1-courseinfo-dev
 set DOCKERFILE=Dockerfile
 
 docker build -f %DOCKERFILE% -t %PROJECT_NAME% . && docker run --rm -it -p 3000:3000 -v .:/app %PROJECT_NAME%
