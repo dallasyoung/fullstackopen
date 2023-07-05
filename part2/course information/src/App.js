956110bc240bc107d5ef3753f2c2064720f11de5
Dallas Young <dallasyoung@outlook.com>
2023-07-05 12:32:08 -0600

    Complete exercise 2.5

--- part2/course information/src/App.js
+++ part2/course information/src/App.js
@@ -1,23 +1,6 @@
+import Course from "./components/Course";
+
 const Header = ({ text }) => <h1>{text}</h1>;
-
-const CourseHeader = ({ coursename }) => <h2>{coursename}</h2>;
-
-const Total = ({ sum }) => <strong><p>total of {sum} exercises</p></strong>;
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>;
-
-const Content = ({ parts }) => (
-  parts.map(p => <Part key={p.id} part={p}/>)
-);
-
-// Exercise 2.3 - Already done!
-const Course = ({course}) => (
-  <div>
-    <CourseHeader coursename={course.name} />
-    <Content parts={course.parts} />
-    <Total sum={course.parts.reduce((a, p) => a + p.exercises, 0)} />
-  </div>
-);
 
 const App = () => {
   const COURSES = [
