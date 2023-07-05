fcd3340d76afb3b97d91a3e70e2ef3ab278ebe39
Dallas Young <dallasyoung@outlook.com>
2023-07-05 12:34:06 -0600

    Update `index.js` code & React 17 -> 18

--- part2/course information/src/index.js
+++ part2/course information/src/index.js
@@ -1,7 +1,15 @@
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
+import App from './App';
+
+// Didn't notice until now that this code from the model answers was quite out
+// of date, and trying to update this caused all sorts of headaches with out of
+// date React requirements, dependency conflicts, etc. Was eventually able to
+// get this sorted by updating from React 17 -> 18, deleting the package lock &
+// ./node_modules, and re-installing all required packages
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
