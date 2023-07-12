207543b969fcee386e596971e3c55ad94feccf29
Dallas Young <dallasyoung@outlook.com>
2023-07-12 13:25:37 -0600

    Complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import SearchFilter from "./components/SearchFilter";
@@ -6,18 +7,19 @@ import AddUserForm from "./components/AddUserForm";
 import Phonebook from "./components/Phonebook";
 
 const App = () => {
-  const [persons, setPersons] = useState(
-    [
-      { name: "Arto Hellas", number: "040-1234567" },
-      { name: 'Ada Lovelace', number: '39-44-5323523' },
-      { name: 'Dan Abramov', number: '12-43-234345' },
-      { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]
-  );
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
   
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+      });
+  }, []);
+
   const addPerson = event => {
     event.preventDefault();
     const warnString = `${newNumber} is already added to phonebook`;
