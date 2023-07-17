53daa8a1c0b695585242e51d9fe5b66c5f975a62
Dallas Young <dallasyoung@outlook.com>
2023-07-17 12:40:12 -0600

    Add scripts for countrydata

--- part2/countrydata/run.bat
+++ part2/countrydata/run.bat
@@ -1,4 +1,4 @@
-set PROJECT_NAME=fullstackopen-part2-countrydata
+set PROJECT_NAME=fullstackopen-part2-countrydata-dev
 set DOCKERFILE=Dockerfile
 
-docker build -f %DOCKERFILE% -t %PROJECT_NAME% . && docker run --rm -it -p 3000:3000 %PROJECT_NAME%
+docker build -f %DOCKERFILE% -t %PROJECT_NAME% . && docker run --rm -it -p 3000:3000 -v .:/app %PROJECT_NAME%
