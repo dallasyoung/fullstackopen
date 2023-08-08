40db9d6f93093a36be0fe72bf78d6e29890779b1
Dallas Young <dallasyoung@outlook.com>
2023-08-08 17:57:17 -0600

    Complete exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,4 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
-module.exports = dummy;
+const totalLikes = (blogs) => blogs.reduce((count, blog) => count + blog.likes, 0);
+
+module.exports = {
+    dummy,
+    totalLikes
+};
